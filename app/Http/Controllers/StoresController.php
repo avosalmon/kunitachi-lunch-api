fee06433ce297e5c3f84ee2474306e93dab30e8d
Ryuta Hamasaki <avosalmon@gmail.com>
2017-05-05 11:40:12 +0900

    implement StoresController@index

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -2,7 +2,122 @@
 
 namespace App\Http\Controllers;
 
-class Controller extends Controller
-{
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use App\Http\Models\Stores\Stores;
 
+class StoresController extends Controller
+{
+    /**
+     * Request class instance.
+     *
+     * @var Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * The ResponseFactory class instance.
+     *
+     * @var Illuminate\Contracts\Routing\ResponseFactory;
+     */
+    protected $response;
+
+    /**
+     * Stores model instance.
+     *
+     * @var App\Http\Models\Stores\Stores
+     */
+    protected $stores;
+
+    /**
+     * Create new instances for dependencies.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param Illuminate\Contracts\Routing\ResponseFactory;
+     * @param App\Http\Models\Stores\Stores $stores
+     * @return void
+     */
+    public function __construct(Request $request, ResponseFactory $response, Stores $stores)
+    {
+        $this->request  = $request;
+        $this->response = $response;
+        $this->stores   = $stores;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $this->stores->setOffset((int)$this->request->input('offset'));
+        $this->stores->setLimit((int)$this->request->input('limit'));
+        $this->stores->setSort($this->request->input('sort'));
+        $this->stores->setDirection($this->request->input('direction'));
+
+        $stores = $this->stores->all();
+        $total  = $this->stores->count();
+        $meta   = $this->generateResponseMeta($this->stores, $total);
+
+        return $this->response->json($this->formatResponse($type = 'stores', $stores, $meta));
+    }
+
+    /**
+     * Display a listing of the resource with nested relationships.
+     *
+     * @param string $relationships
+     *
+     * @return Response
+     */
+    public function with($relationships)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function update($id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+
+    }
 }
