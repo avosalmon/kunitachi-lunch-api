35d2b3415a60b77df178b19bec7e2e2eb1f5822b
Ryuta Hamasaki <avosalmon@gmail.com>
2017-05-27 14:49:46 +0900

    implement store-image relationship

---
+++ app/Models/Images/ImagesEloquent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Images;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImagesEloquent extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'images';
+}
--- app/Models/Stores/StoresEloquent.php
+++ app/Models/Stores/StoresEloquent.php
@@ -22,4 +22,14 @@ class StoresEloquent extends Model
     {
         return $this->belongsToMany('App\Models\Tags\TagsEloquent', 'store_tags', 'store_id', 'tag_id')->orderBy('display_order', 'asc')->withTimestamps();
     }
+
+    /**
+     * Get images which are related to this product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function images()
+    {
+        return $this->belongsToMany('App\Models\Images\ImagesEloquent', 'store_images', 'store_id', 'image_id')->withTimestamps();
+    }
 }
